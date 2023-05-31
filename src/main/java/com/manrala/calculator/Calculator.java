aff332c6b02cdd6d8af451cc88c280747b80a026
mannamnaveen <mina@naveenmannam.com>
2023-05-31 20:05:31 +0000

    Added test stage to Java

--- src/main/java/com/manrala/calculator/Calculator.java
+++ src/main/java/com/manrala/calculator/Calculator.java
@@ -2,12 +2,9 @@ package com.manrala.calculator;
 
 import org.springframework.stereotype.Service;
 
-public
 @Service
 public class Calculator {
 	public int sum(int a, int b) {
 		return a + b;
 	}
-}{
-
 }
